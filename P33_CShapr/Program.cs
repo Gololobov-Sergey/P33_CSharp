6a5867dfea42d9b46f8afb5508e7468d5a5eec2a
Сергій Гололобов <gololobov.serhiy@gmail.com>
2025-01-18 14:39:01 +0200

    Добавьте файлы проекта.

---
+++ P33_CShapr/Program.cs
@@ -0,0 +1,78 @@
+﻿using System.Text;
+
+namespace P33_CShapr
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "P33 C#";
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.InputEncoding = Encoding.UTF8;
+            Console.Clear();
+
+           
+            Console.WriteLine("Слава Україні!");
+
+            //Int32 a1 = 5;
+            //int a = 5;
+
+            //float a2 = 2.22f;
+
+            //DateTime a3 = DateTime.Now;
+            //Console.WriteLine(a3.DayOfYear);
+            //Console.WriteLine(a3.ToLongDateString());
+            //Console.WriteLine(a3.ToShortDateString());
+
+            //Console.WriteLine($"{a2} info {a3.ToLongDateString()}");
+
+            //int c = Convert.ToInt32(Console.ReadLine()); 
+            //int d = Int32.Parse(Console.ReadLine()!);
+
+            //DateTime d2 = Convert.ToDateTime(Console.ReadLine()!);
+            //Console.WriteLine(d2);
+
+            //int? s = null;
+            //string? v = null;
+
+            //if(a != 0)
+            //{
+            //    Console.WriteLine("True");
+            //}
+
+            //int m = a > 0 ? 1 : 0;
+
+            //switch (m)
+            //{
+            //    case 0:
+            //        Console.WriteLine();
+            //        break;
+            //    case 1:
+            //    case 2:
+            //        Console.WriteLine(3);
+            //        break;
+            //    default:
+            //        break;
+            //}
+
+            //for (int i = 0; i < 3; i++)
+            //{
+
+            //}
+
+            //while (true)
+            //{
+
+            //}
+
+            //do {
+
+
+            //}while(true);
+
+            Console.ReadLine();
+        }
+    }
+}

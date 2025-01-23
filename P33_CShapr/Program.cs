9e21727c0374e3317cc6e913801a949ae0528c8b
Сергій Гололобов <gololobov.serhiy@gmail.com>
2025-01-23 20:49:13 +0200

    23.01.2025

--- P33_CShapr/Program.cs
+++ P33_CShapr/Program.cs
@@ -13,8 +13,120 @@ namespace P33_CShapr
             Console.InputEncoding = Encoding.UTF8;
             Console.Clear();
 
-           
+
             Console.WriteLine("Слава Україні!");
+
+            /// 23.01.2025 ///
+            /// /////////////
+
+
+            //int[] arr1 = new int[5] { 3, 6, 4, 6, 9 };
+            //int[] arr2 = { 3, 6, 4, 6, 9 };
+            //int[] arr3;
+            //arr3 = new int[] { 3, 6, 4, 6, 9 };
+
+            //for (int i = 0; i < arr1.Length; i++)
+            //{
+            //    //arr1[i] *= 2;
+            //    Console.Write($"{arr1[i]} ");
+            //}
+            //Console.WriteLine();
+
+            //foreach (int i in arr1)
+            //{
+            //    //i *= 2;
+            //    Console.Write($"{i} ");
+            //}
+            //Console.WriteLine();
+
+            //Console.WriteLine(arr1.Count(a => a > 4));
+
+            //Console.WriteLine(arr1.Sum());
+
+            //Array.Sort(arr1);
+            //foreach (int i in arr1)
+            //{
+            //    //i *= 2;
+            //    Console.Write($"{i} ");
+            //}
+            //Console.WriteLine();
+
+            //int[,] arr5 = new int[2, 3] { { 3, 5, 7 }, { 4, 2, 1 } };
+            //Console.WriteLine(arr5[1,2]);
+            //for (int i = 0; i < arr5.GetLength(0); i++)
+            //{
+            //    for (int j = 0; j < arr5.GetLength(1); j++)
+            //    {
+            //        Console.Write(arr5[i,j] + " ");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+
+            //int[][] arr6 = new int[2][];
+            //arr6[0] = new int[3] { 6,7,8};
+            //arr6[1] = new int[4] { 1,2,3,4};
+
+            //for (int i = 0; i < arr6.GetLength(0); i++)
+            //{
+            //    foreach(int a in arr6[i])
+            //    {
+            //        Console.Write(a + " ");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            //Console.Write("size = ");
+            //int size = Convert.ToInt32(Console.ReadLine());
+            //int[] arr = new int[size];  
+            //Random random = new Random();
+            //for (int i = 0; i < size; i++)
+            //{
+            //    arr[i] = random.Next(0, 10);
+            //    Console.Write(arr[i] + " ");
+            //}
+            //Console.WriteLine();
+
+            //Console.WriteLine(arr.Count(a => a % 2 == 0));
+            //Console.WriteLine(arr.Count(a => a % 2 == 1));
+            //Console.WriteLine(arr.Distinct().Count());
+
+
+            //Console.Write("size1 = ");
+            //int size1 = Convert.ToInt32(Console.ReadLine());
+            //Console.Write("size2 = ");
+            //int size2 = Convert.ToInt32(Console.ReadLine());
+            //int[] arr1 = new int[size1];
+            //Random random = new Random();
+            //for (int i = 0; i < size1; i++)
+            //{
+            //    arr1[i] = random.Next(0, 10);
+            //    Console.Write(arr1[i] + " ");
+            //}
+            //Console.WriteLine();
+
+            //int[] arr2 = new int[size2];
+            //for (int i = 0; i < size2; i++)
+            //{
+            //    arr2[i] = random.Next(0, 10);
+            //    Console.Write(arr2[i] + " ");
+            //}
+            //Console.WriteLine();
+
+            //int[] arr3 = arr1.Intersect(arr2).ToArray();
+            //foreach (int i in arr3)
+            //{
+            //    Console.Write(i + " ");
+            //}
+            //Console.WriteLine();
+
+
+            string st = "mama";
+            st += "papa";
+
+            StringBuilder sb = new StringBuilder();
+            
+
 
             //Int32 a1 = 5;
             //int a = 5;
